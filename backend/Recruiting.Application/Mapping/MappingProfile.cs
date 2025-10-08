03408b7c5a74edb8eb2414c6c89264c9fecaaa4b
almer-hodzic <137727738+almer-hodzic@users.noreply.github.com>
2025-10-08 12:23:47 +0200

    feat(mapping): add AutoMapper MappingProfile for User entity and DTOs

---
+++ backend/Recruiting.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Recruiting.Application.DTOs;
+using Recruiting.Domain.Entities;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Recruiting.Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDto>().ReverseMap();
+            CreateMap<CreateUserDto, User>();
+        }
+    }
+}
+
