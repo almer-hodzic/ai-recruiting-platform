5ff40dc302ef68dac16fdafe2125b2aadd7021a4
almer-hodzic <137727738+almer-hodzic@users.noreply.github.com>
2025-10-08 12:23:30 +0200

    feat(interfaces): define IUserService interface

---
+++ backend/Recruiting.Application/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using Recruiting.Application.DTOs;
+
+namespace Recruiting.Application.Interfaces
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserDto>> GetAllAsync();
+        Task<UserDto?> GetByIdAsync(Guid id);
+        Task<UserDto> CreateAsync(CreateUserDto createUserDto);
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
